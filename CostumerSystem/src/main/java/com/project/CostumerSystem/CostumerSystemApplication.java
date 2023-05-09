ef6692c14b9cfede3cf942c8dd85aee666208c30
Jonas Felix <jonaspetty@hotmail.com>
2023-05-08 21:36:21 -0300

    add: creating the spring project

---
+++ CostumerSystem/src/main/java/com/project/CostumerSystem/CostumerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.CostumerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerSystemApplication.class, args);
+	}
+
+}
