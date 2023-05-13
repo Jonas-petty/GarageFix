be3ddb2eaa57bf075fc3ba88994bf12889d7e346
Jonas Felix <jonaspetty@hotmail.com>
2023-05-12 21:42:55 -0300

    Add: adding the seed to the table costumer

--- CostumerSystem/src/main/resources/import.sql
+++ CostumerSystem/src/main/resources/import.sql
@@ -4,3 +4,10 @@ INSERT INTO service (name, description, price) VALUES ('DVD Player Repair', 'Con
 INSERT INTO service (name, description, price) VALUES ('Sound System Repair', 'Reparo em sistemas de som', 120.00);
 INSERT INTO service (name, description, price) VALUES ('Home Theater Repair', 'Serviço de conserto para home theaters', 180.00);
 INSERT INTO service (name, description, price) VALUES ('Electronic Equipment Repair', 'Manutenção e reparo em diversos aparelhos eletrônicos', 100.00);
+
+INSERT INTO costumer (name, phone_number, service_id) VALUES ('João Silva', '1234567890', 1);
+INSERT INTO costumer (name, phone_number, service_id) VALUES ('Maria Santos', '9876543210', 3);
+INSERT INTO costumer (name, phone_number, service_id) VALUES ('Pedro Oliveira', '5555555555', 2);
+INSERT INTO costumer (name, phone_number, service_id) VALUES ('Ana Costa', '9999999999', 4);
+INSERT INTO costumer (name, phone_number, service_id) VALUES ('Carlos Pereira', '1111111111', 6);
+INSERT INTO costumer (name, phone_number, service_id) VALUES ('Laura Souza', '4444444444', 5);
