5d498a6863cc9539dd062f104a2575f349210b21
Jonas Felix <jonaspetty@hotmail.com>
2023-05-12 21:34:49 -0300

    Add: adding the seed to the table service

---
+++ CostumerSystem/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO service (name, description, price) VALUES ('TV Repair', 'Serviço de conserto para televisões', 150.00);
+INSERT INTO service (name, description, price) VALUES ('Radio Repair', 'Reparo e manutenção de rádios', 80.00);
+INSERT INTO service (name, description, price) VALUES ('DVD Player Repair', 'Conserto de aparelhos de DVD', 100.00);
+INSERT INTO service (name, description, price) VALUES ('Sound System Repair', 'Reparo em sistemas de som', 120.00);
+INSERT INTO service (name, description, price) VALUES ('Home Theater Repair', 'Serviço de conserto para home theaters', 180.00);
+INSERT INTO service (name, description, price) VALUES ('Electronic Equipment Repair', 'Manutenção e reparo em diversos aparelhos eletrônicos', 100.00);
