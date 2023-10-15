ef3d951762b20bb022044c7e315e01b9590e76d3
Jonas Felix <jonaspetty@hotmail.com>
2023-10-15 00:50:25 -0300

    new project's first commit

---
+++ garagefix_backend/src/main/java/com/jonas/project/garagefix/GaragefixApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.project.garagefix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragefixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragefixApplication.class, args);
+	}
+
+}
---
+++ garagefix_backend/src/test/java/com/jonas/project/garagefix/GaragefixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.project.garagefix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragefixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
