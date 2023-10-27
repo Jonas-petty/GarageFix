3dcf6fb0bf828417d64847c862201db7614d3c5a
Jonas Felix <jonaspetty@hotmail.com>
2023-10-27 00:31:57 -0300

    configuring basic CORS and fixing some bugs

--- garagefix_backend/src/main/java/com/jonas/project/garagefix/entity/client/ClientDetailsData.java
+++ garagefix_backend/src/main/java/com/jonas/project/garagefix/entity/client/ClientDetailsData.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 public record ClientDetailsData(
         UUID id,
-        String nome,
+        String name,
         String phoneNumber,
         LocalDateTime createdAt
 ) {
